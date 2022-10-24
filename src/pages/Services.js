f71ceec2a59b6e865b8214e2917459f748034a28
shahadat2772 <shahadat2772@gmailcom>
2022-10-24 08:31:36 +0600

    added axios and connected to db

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import fetcher from "../api";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+  console.log(services);
+
+  useEffect(() => {
+    // axios
+    // .get("http://localhost:5000/service")
+    // .then((res) => setServices(res.data));
+
+    // This is how an iiv
+    (async () => {
+      const res = await fetcher.get("http://localhost:5000/service");
+      setServices(res.data);
+    })();
+  }, []);
+
   return (
     <div>
       <h1>This is services</h1>
